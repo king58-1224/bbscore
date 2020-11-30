1008c82ae35ccdb86fd9db57c15415c0571efcd8
yuta-satou <68938732+yuta-satou@users.noreply.github.com>
2020-12-01 07:44:22 +0900

    選手登録の実装

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,10 +1,51 @@
 class MembersController < ApplicationController
-	def index
+  def index
+    @members = Member.all
 	end
 
 	def new
-	end
+    @member = Member.new
+  end
 
-	def edit
-	end
+  def create
+    @member = Member.new(member_params)
+    if @member.valid?
+      @member.save
+      return redirect_to members_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @member = Member.find(params[:id])
+  end
+
+  def update
+    @member = Member.find(params[:id])
+    if @member.update(member_params)
+      return redirect_to members_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @member = Member.find(params[:id])
+    if @member.destroy
+      return redirect_to members_path
+    else
+      render :index
+    end
+  end
+
+
+
+  private
+
+  def member_params
+    params.require(:member).permit(:uniform_number, :grade, :first_name,:last_name).merge(team_id: current_team.id)
+  end
+
+
 end
