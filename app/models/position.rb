9e2791fbfed7f6ee8043bc11ba32b59f891f34b3
yuta-satou <68938732+yuta-satou@users.noreply.github.com>
2020-12-10 21:54:18 +0900

    試合登録の実装

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,2 +1,30 @@
-class Position < ApplicationRecord
+class Position < ActiveHash::Base
+  self.data = [
+   { id: 1, name: '--' },
+   { id: 2, name: '投手' },
+   { id: 3, name: '捕手' },
+   { id: 4, name: '一塁手' },
+   { id: 5, name: '二塁手' },
+   { id: 6, name: '三塁手' },
+   { id: 7, name: '遊撃手' },
+   { id: 8, name: '左翼手' },
+   { id: 9, name: '中堅手' },
+   { id: 10, name: '右翼手' }
+  ]
+
+  # self.data = [
+  #  { id: 1, name: '--' },
+  #  { id: 2, name: '投' },
+  #  { id: 3, name: '捕' },
+  #  { id: 4, name: '一' },
+  #  { id: 5, name: '二' },
+  #  { id: 6, name: '三' },
+  #  { id: 7, name: '遊' },
+  #  { id: 8, name: '左' },
+  #  { id: 9, name: '中' },
+  #  { id: 10, name: '右' }
+  # ]
+
+  include ActiveHash::Associations
+  has_many :batting_orders
 end
